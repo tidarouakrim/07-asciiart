0bd236480f3100613e98c7b374e3434c196e688c
tidar-ouakrim <tidar.ouakrim@edu.esiee.fr>
2025-10-03 08:41:25 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-#### Imports et définition des variables globales
+'encoder une chaine de caractères par une liste de tuples'
 
 # Mandatory for the recursive solution to work on large inputs
 import sys
-sys.setrecursionlimit(2000)
+sys.setrecursionlimit(10000)
 
 
 #### Fonctions secondaires
 
 
 def artcode_i(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme itératif
+    """retourne la liste de tuples selon un algorithme itératif
 
     Args:
         s (str): la chaîne de caractères à encoder
@@ -17,14 +17,23 @@ def artcode_i(s):
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
-    # votre code ici
 
-    return [ ]
+    c = [s[0]]  # liste des caractères rencontrés
+    o = [1]     # liste des occurrences correspondantes
+    k = 1       # index courant
+    n = len(s)
+    while k < n:
+        if s[k] == c[-1]:
+            o[-1] += 1
+        else:
+            c.append(s[k])
+            o.append(1)
+        k += 1
+    return list(zip(c, o))
 
 
 def artcode_r(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme récursif
+    """retourne la liste de tuples selon un algorithme récursif
 
     Args:
         s (str): la chaîne de caractères à encoder
@@ -32,20 +41,26 @@ def artcode_r(s):
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
-    # votre code ici
+
+    def f(rest, c, n):
+        if not rest:
+            return [(c, n)]
+        if rest[0] == c:
+            return f(rest[1:], c, n + 1)
+        return [(c, n)] + f(rest[1:], rest[0], 1)
+
+    return f(s[1:], s[0], 1)
 
     # cas de base
     # recherche nombre de caractères identiques au premier
     # appel récursif
 
-    return []
-    
 
 #### Fonction principale
 
 
 def main():
+    ' appel aux fonctions secondaires'
     print(artcode_i('MMMMaaacXolloMM'))
     print(artcode_r('MMMMaaacXolloMM'))
 
